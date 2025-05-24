c06af550e29e7501614d2df8a44c1538d2d003c3
Varad Kadam <kadam.varad@protonmail.com>
2025-05-25 00:29:40 +0530

    Updated configs for Github Pages

--- vite.config.js
+++ vite.config.js
@@ -3,8 +3,9 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/medblocks',
   plugins: [react()],
   optimizeDeps: {
     exclude: ['@electric-sql/pglite'],
-  },
+  }, 
 })
