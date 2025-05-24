c21429ae5d933828066de45432481d3201f5ea4e
Varad Kadam <kadam.varad@protonmail.com>
2025-05-24 20:18:42 +0530

    Completed Form and JS validations for fields: Email, MRN, DOB

--- src/components/InputForm/InputForm.jsx
+++ src/components/InputForm/InputForm.jsx
@@ -17,7 +17,19 @@ const defaultValues = {
     mrn: '',
     registerDate: ''
 }
+
 function InputForm() {
+
+    const [formErrors, setformErrors] = useState({})
+    const [isSubmit, setisSubmit] = useState(false)
+
+    useEffect(() => {
+        console.log(formErrors)
+        if (Object.keys(formErrors) === 0 && isSubmit) {
+            console.log(values)
+        }
+
+    }, [formErrors])
 
     // Initialize from localStorage if available
     const [values, setValues] = useState(() => {
@@ -25,12 +37,11 @@ function InputForm() {
         return saved ? JSON.parse(saved) : defaultValues
     })
 
-
     useEffect(() => {
         localStorage.setItem(STORAGE_KEY, JSON.stringify(values))
     }, [values])
 
-
+    // Update localStorage when values change
     const handleChange = (e) => {
         const { name, value, type, checked } = e.target
         setValues(prev => ({
@@ -42,19 +53,60 @@ function InputForm() {
     // Reset form to default values
     const handleReset = () => {
         setValues(defaultValues)
-        // localStorage.removeItem(STORAGE_KEY)
+        localStorage.removeItem(STORAGE_KEY)
+
+        setformErrors({})
+        setisSubmit(false)
     }
 
+    // Form submit actions
     const handleSubmit = (e) => {
         e.preventDefault()
         console.log('Submitted:', values)
+
+        setformErrors(validateForm(values))
+        setisSubmit(true)
+    }
+
+    // Form validation actions
+    const validateForm = (values) => {
+        const errors = {}
+        const emailRegex = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
+        const today = new Date()
+        const dob = new Date(values.dob)
+
+        if (values.email && !emailRegex.test(values.email)) {
+                errors.email = "This is not a valid email."
+        }
+
+        if (values.mrn.length != 8) {
+            errors.mrn = "MRN must be 8 charaters long."
+        }
+
+        if (values.dob){
+            if(dob > today){
+                errors.dob = "Date of Birth cannot be in the future."
+            }
+            else {
+                const yearsAgo = today.getFullYear() - dob.getFullYear()
+                if(yearsAgo > 150){
+                    errors.dob = "Date of Birth cannot be greater than 150 years"
+                }
+            }
+            
+        }
+
+        // Similarly for Register Date, Phone, etc.
+        
+
+        return errors
     }
 
     return (
         <div className="inputForm">
-            <form>
-                <label htmlFor="firstName"> First Name: </label>
-                <input type="text" placeholder="Patient's First Name" name='firstName'
+            <form onSubmit={handleSubmit}>
+                <label htmlFor="firstName"> First Name *: </label>
+                <input type="text" placeholder="Patient's First Name" name='firstName' required
                     value={values.firstName}
                     onChange={handleChange}
                 />
@@ -65,19 +117,23 @@ function InputForm() {
                     onChange={handleChange}
                 />
 
-                <label htmlFor="dob"> Date of Birth: </label>
-                <input type="date" name='dob'
+                <label htmlFor="dob"> Date of Birth *: </label>
+                <input type="date" name='dob' required
                     value={values.dob}
                     onChange={handleChange}
                 />
+                <p style={{ marginTop: '0', marginBottom: '1rem', color: 'red', fontSize: '0.875rem' }}>
+                    {formErrors.dob}
+                </p>
 
-                <label>Sex:</label>
+                <label>Sex *:</label>
                 <div className="radio-group">
                     <label>
                         <input
                             type="radio"
                             name="sex"
                             value="Male"
+                            required
                             checked={values.sex === 'Male'}
                             onChange={handleChange}
                         /> Male
@@ -87,6 +143,7 @@ function InputForm() {
                             type="radio"
                             name="sex"
                             value="Female"
+                            required
                             checked={values.sex === 'Female'}
                             onChange={handleChange}
                         /> Female
@@ -96,6 +153,7 @@ function InputForm() {
                             type="radio"
                             name="sex"
                             value="Other"
+                            required
                             checked={values.sex === 'Other'}
                             onChange={handleChange}
                         /> Other
@@ -113,6 +171,9 @@ function InputForm() {
                     value={values.email}
                     onChange={handleChange}
                 />
+                <p style={{ marginTop: '0', marginBottom: '1rem', color: 'red', fontSize: '0.875rem' }}>
+                    {formErrors.email}
+                </p>
 
                 <label htmlFor="address"> Address: </label>
                 <input type="text" placeholder="Patient's Address" name='address'
@@ -138,17 +199,25 @@ function InputForm() {
                     onChange={handleChange}
                 />
 
-                <label htmlFor="mrn"> Medical Record Number: </label>
-                <input type="text" placeholder="Patient's MRN" name='mrn'
+                <label htmlFor="mrn"> Medical Record Number *: </label>
+                <input type="text" placeholder="Patient's MRN" name='mrn' required
                     value={values.mrn}
                     onChange={handleChange}
                 />
+                <p style={{ marginTop: '0', marginBottom: '1rem', color: 'red', fontSize: '0.875rem' }}>
+                    {formErrors.mrn}
+                </p>
 
-                <label htmlFor="registerDate"> Registration Date: </label>
-                <input type="date" placeholder="Patient's Registration Date" name='registerDate'
+
+                <label htmlFor="registerDate"> Registration Date *: </label>
+                <input type="date" placeholder="Patient's Registration Date" name='registerDate' required
                     value={values.registerDate}
                     onChange={handleChange}
                 />
+
+                <p>
+                    * Indicates a required value.
+                </p>
 
                 <button type='button' onClick={handleReset}>Reset</button>
                 <button type='submit'>Submit</button>
